8e67b7653dd9ab28f14739c04e7ea45a4f203180
Sergiolfq <sergiolfq@gmail.com>
2020-12-28 11:39:25 -0700

    scaffolding for rest APIS

---
+++ app/Http/Controllers/PollsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Poll;
+use Illuminate\Http\Request;
+use Validator; 
+
+class PollsController extends Controller
+{
+    public function index()
+    {
+    	return response()->json(Poll::get(), 200);
+    }
+
+    public function show($id)
+    {
+    	return response()->json(Poll::findOrFail($id), 200);
+    }
+
+    public function store(Request $request) 
+    {
+    	$rules = [
+    	'title' => 'required|max:10',
+    		
+    	];
+    	$validator = Validator::make($request->all(), $rules);
+    	if($validator->fails()){
+    	    return response()->json($validator->errors(),400);	
+    	}
+    	$poll = Poll::create($request->all());
+    	return response()->json($poll, 201);
+    }
+
+    public function update(Request $request, Poll $poll) 
+    {
+        $poll->update($request->all());
+        return response()->json($poll, 200);
+    }
+    
+    public function delete(Request $request, Poll $poll) 
+    {
+        $poll->delete();
+        return response()->json(null, 204);
+    }
+}
